b2345229f0a5cf20c0819014fa1b31dcc4b6721c
jamesbrond <diego.brondo@philips.com>
2021-11-05 16:35:16 +0100

    feat: add keyring to store todoist api token

--- todo.py
+++ todo.py
@@ -1,11 +1,20 @@
 import sys
 import json
 import uuid
+import keyring
 import logging
 import requests
 from ics import Calendar
 
-API_TOKEN="fe520c257103a0239972e112e1f766d23869389c"
+def setup(service_id):
+	token = input("Please enter your API token: ")
+	set_api_token(service_id, token)
+
+def set_api_token(service_id, token):
+	keyring.set_password(service_id, 'API_TOKEN', token)
+
+def get_api_token(service_id):
+	return keyring.get_password(service_id, 'API_TOKEN')
 
 def read_calendar(in_file):
 	events = []
@@ -19,7 +28,7 @@ def read_calendar(in_file):
 			})
 		return events
 
-def add_task(title, due, label_ids):
+def add_task(api_token, title, due, label_ids):
 	print(f"Add task {title} on {due}...", end="")
 	requests.post(
 		"https://api.todoist.com/rest/v1/tasks",
@@ -31,43 +40,51 @@ def add_task(title, due, label_ids):
 		headers={
 			"Content-Type": "application/json",
 			"X-Request-Id": str(uuid.uuid4()),
-			"Authorization": f"Bearer {API_TOKEN}"
+			"Authorization": f"Bearer {api_token}"
 		}).json()
 	print("\tdone")
 
-def get_all_projects():
+def get_all_projects(api_token):
 	return requests.get(
 		"https://api.todoist.com/rest/v1/projects",
 		headers={
-			"Authorization": f"Bearer {API_TOKEN}"
+			"Authorization": f"Bearer {api_token}"
 		}).json()
 
-def get_all_sections():
+def get_all_sections(api_token):
 	return requests.get(
 		"https://api.todoist.com/rest/v1/sections",
 		headers={
-			"Authorization": f"Bearer {API_TOKEN}"
+			"Authorization": f"Bearer {api_token}"
 		}).json()
 
-def get_all_labels():
+def get_all_labels(api_token):
 	return requests.get(
 		"https://api.todoist.com/rest/v1/labels",
 		headers={
-			"Authorization": f"Bearer {API_TOKEN}"
+			"Authorization": f"Bearer {api_token}"
 		}).json()
 
 def main():
+	service_id = 'JBROND_TODO'
+
 	try:
+		api_token = get_api_token(service_id)
+		while not api_token:
+			logging.warning(f"Todoist API token not find for {service_id} application.")
+			setup(service_id)
+			api_token = get_api_token(service_id)
+
 		if len(sys.argv) < 2:
 			print("\tPROJECTS:")
-			print(get_all_projects())
+			print(get_all_projects(api_token))
 			print("\tSECTIONS:")
-			print(get_all_sections())
+			print(get_all_sections(api_token))
 			print("\tLABELS:")
-			print(get_all_labels())
+			print(get_all_labels(api_token))
 		else:
 			for event in read_calendar(sys.argv[1]):
-				add_task(event['title'], event['date'], [2158774081])
+				add_task(api_token, event['title'], event['date'], [2158774081])
 		return 0
 	except Exception as e:
 		logging.error(e, exc_info=True)
