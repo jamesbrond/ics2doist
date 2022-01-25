e4c73504dfbcbab70a677a76f134e3000d90fd9a
jamesbrond <diego.brondo@philips.com>
2022-01-25 16:40:19 +0100

    refactor: use todoist api to perform backup; move all backup ad restore stuff in a single class

--- doistbackup.py
+++ doistbackup.py
@@ -3,7 +3,6 @@ import logging
 import argparse
 import lib.key_ring as keyring
 from lib.doistbackup import DoistBackup
-from lib.doistrestore import DoistRestore
 
 def parse_cmd_line():
 	parser = argparse.ArgumentParser(prog="doistbackup.py", usage='%(prog)s [options]', description='Backup and restore utility for Todoist')
@@ -47,11 +46,10 @@ def main():
 			keyring.setup(service_id)
 			api_token = keyring.get_api_token(service_id)
 
+		bkg = DoistBackup(args.filename, api_token)
 		if args.is_restore:
-			rest = DoistRestore(args.filename, api_token)
-			rest.restore()
+			bkg.restore()
 		elif args.is_backup:
-			bkg = DoistBackup(args.filename, api_token)
 			bkg.backup()
 
 		return 0
--- lib/doistbackup.py
+++ lib/doistbackup.py
@@ -1,34 +1,49 @@
 import gzip
+import json
 import logging
-import requests
+import todoist
 from datetime import datetime
 
-TODOIST_SYNC_URL = 'https://api.todoist.com/sync/v8/sync'
+def state_default(obj):
+	return obj.data
 
 class DoistBackup:
 
 	def __init__(self, filename, api_token):
-		self.token = api_token
-		self.bkgfile = f"{filename}-{datetime.now().strftime('%Y%m%d%H%M%S')}.bak.gz"
+		self.filename = filename
+		self.api = todoist.TodoistAPI(api_token)
 
 	def backup(self):
-		self.write_file(self.sync())
+		bkgfile = f"{self.filename}-{datetime.now().strftime('%Y%m%d%H%M%S')}.bak.gz"
+		self.write_file(bkgfile, self.sync())
 
-	def write_file(self, content):
-		logging.info(f"write to {self.bkgfile}")
-		with gzip.open(self.bkgfile, 'wb') as f:
+	def restore(self):
+		bkgfile = self.filename
+		self.sync(self.read_file(bkgfile))
+
+	def read_file(self, bkgfile):
+		logging.info(f"read from {bkgfile}")
+		with gzip.open(bkgfile , 'rb') as f:
+			content = f.read()
+		logging.debug(f"read {len(content)} bytes")
+		return content
+
+	def write_file(self, bkgfile, content):
+		logging.info(f"write to {bkgfile}")
+		with gzip.open(bkgfile, 'wb') as f:
 			f.write(content)
-		logging.debug(f"writed {len(content)} bytes")
+		logging.debug(f"wrote {len(content)} bytes")
 
-	def sync(self):
-		headers = {"Authorization": f"Bearer {self.token}"}
-		params = {
-			"sync_token": "*",
-			"resource_types": '["all"]'
-		}
-		resp = requests.get(TODOIST_SYNC_URL, headers=headers, params=params)
-		if resp.status_code == 200:
-			return resp.content
-		return ""
+	def sync(self, content=None):
+		if content is None:
+			return bytes(json.dumps(self.api.state, default=state_default), 'utf8')
+		else:
+			self.api.reset_state()
+			self.api._update_state(json.loads(content.decode('utf8')))
+			self.api._write_cache()
+			raise NotImplementedError("Not implemented yet because sync doesn't update server")
+			# resp = self.api.sync()
+			# print(bytes(json.dumps(resp), 'utf8'))
+			# logging.debug(resp)
 
 # ~@:-]
